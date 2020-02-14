e0fcd3ff0ed280037aa92dca8572ab226a16c00e
HKMASTER <HKMASTER@LAPTOP-0JGDFE4E>
2020-02-14 11:32:36 +0900

    세번째...

---
+++ sp4-test01/src/main/java/sp4/sp4_test01/MainTest.java
@@ -0,0 +1,13 @@
+package sp4.sp4_test01;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter gt = new Greeter();
+		gt.setFormat("%s, 안녕하세요!");
+		String msg = gt.greet("이숭무");
+		System.out.println(msg);
+	}
+
+}

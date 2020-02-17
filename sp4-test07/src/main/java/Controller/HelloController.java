91629644810a0e35b885ce373b26653b09c45578
HKMASTER <HKMASTER@LAPTOP-0JGDFE4E>
2020-02-17 14:53:45 +0900

    fvsdfsfsaf

---
+++ sp4-test07/src/main/java/Controller/HelloController.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HelloController {
+	@RequestMapping("/hello")
+	public String helloTest(Model model,
+			@RequestParam(value="name", required=false) int a ) {
+		model.addAttribute("test", a);
+		return "hello";
+	}
+}
